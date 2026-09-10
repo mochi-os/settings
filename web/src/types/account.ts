7138d953a68f81d3de887403153af942850323a6
Alistair Cunningham <acunningham@integrics.com>
2026-09-10 14:14:37 +0100

    Remove the nine unused template dependencies and un-export Identity and SystemSettingsData, which only their own modules use (#1331, #1332)

--- web/src/types/account.ts
+++ web/src/types/account.ts
@@ -3,7 +3,7 @@
 // This file is part of Mochi, licensed under the GNU AGPL v3 with the
 // Mochi Application Interface Exception - see license.txt and license-exception.md.
 
-export interface Identity {
+interface Identity {
   entity: string
   fingerprint: string
   username: string
--- web/src/types/settings.ts
+++ web/src/types/settings.ts
@@ -3,4 +3,4 @@
 // This file is part of Mochi, licensed under the GNU AGPL v3 with the
 // Mochi Application Interface Exception - see license.txt and license-exception.md.
 
-export type { SystemSetting, SystemSettingsData } from '@mochi/web'
+export type { SystemSetting } from '@mochi/web'
